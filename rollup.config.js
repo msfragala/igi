1fe5ba423076cbb350778c7c48f0f113ac6f595b
Mitchell Fragala <ms.fragala@gmail.com>
2020-10-01 07:32:50 -0400

    Remove terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 import node from '@rollup/plugin-node-resolve';
 import externals from 'rollup-plugin-node-externals';
 
@@ -11,7 +10,7 @@ export default {
       dir: 'dist',
       format: 'cjs',
    },
-   plugins: [typescript(), json(), node(), commonjs(), terser(), externals()],
+   plugins: [typescript(), json(), node(), commonjs(), externals()],
    onwarn(warning, warn) {
       if (warning.code !== 'CIRCULAR_DEPENDENCY') {
          warn(warning);
